131f7e238745415a983faa08feb54b73065e1dd5
Kevin Kyle <kevinkyle314@gmail.com>
2025-10-20 22:51:16 -0400

    moving scripts to folder

--- call_script.m
+++
@@ -1,6 +0,0 @@
-clear;
-vtkFile = 'vsurf00010000.vtk';
-inputFile = 'input (1).sdf';
-[P_inf, T_inf, M_inf, rho_inf, pran, y, Rgas] = load_input(inputFile);
-[T_e, P_e, U_e, V_e, W_e, centroids, norms, areas] = load_vtk_surf(vtkFile);
-
--- exportHeatFlux.m
+++
@@ -1,9 +0,0 @@
-data = [centroids, q_w];
-filename = 'heatflux_data.csv';
-% Write header
-header = {'x', 'y', 'z', 'q_w'};
-fid = fopen(filename,'w');
-fprintf(fid,'%s,%s,%s,%s\n', header{:});
-fclose(fid);
-writematrix(data, filename, 'WriteMode', 'append');
-disp(['CSV file saved to ', filename])
